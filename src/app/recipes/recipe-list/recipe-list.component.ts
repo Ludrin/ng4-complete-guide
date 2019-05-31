243bbd53f0555dbfa8719e419b6129bfa0bc5b13
Ernani Ricoy <ernani.ricoy@engdb.com.br>
2019-05-31 15:49:18 -0300

    Angular 8 course - Section 3
    - Create basic Angular 8 project with Angular CLI.
    - Create recipe, recipe-detail, recpe-item and recipe-list components.
    - Create shopping-list and shopping-edit components.
    - Create recipe and ingredient models.
    - Create simple representations of how all pages should look like.
    There is no routing or page switching yet. This is simply the initial
    building blocks.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Pork 'n veggies",
+      'Sliced pork with vegetables',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+    new Recipe(
+      'Simple tagliatelle',
+      'Tagliatelle with grinded cheese',
+      'https://c.pxhere.com/photos/99/04/tagliatelle_pasta_on_a_fork_food_italian_meal_cuisine_italy_traditional-1375412.jpg!d'
+    )
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
