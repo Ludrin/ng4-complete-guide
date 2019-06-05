fbe7cf9421e39bb7f7138e4f20d27b077b473141
Ernani Ricoy <ernani.ricoy@engdb.com.br>
2019-06-05 11:17:25 -0300

    Angular 8 course - Section 6
    - Create simple navigation between pages using event emiters.
    - Create simple add ingredient in shopping list using ViewChild and
    event emiter.
    These functionalities will be replaced after the routing and form
    sections.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showRecipes = false;
+  showShoppingList = false;
 
+  onRecipesToggle(recipesState: boolean) {
+    this.showRecipes = recipesState;
+  }
+
+  onShoppingListToggle(shoppingListState: boolean) {
+    this.showShoppingList = shoppingListState;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() toggleRecipes = new EventEmitter<boolean>();
+  @Output() toggleShoppingList = new EventEmitter<boolean>();
+
+  showRecipes = false;
+  showShoppingList = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipesClick() {
+    this.showRecipes = !this.showRecipes;
+    this.toggleRecipes.emit(this.showRecipes);
+  }
+
+  onShoppingListClick() {
+    this.showShoppingList = !this.showShoppingList;
+    this.toggleShoppingList.emit(this.showShoppingList);
   }
 
 }
