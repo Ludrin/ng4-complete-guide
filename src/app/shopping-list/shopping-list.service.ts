2e36a2327ce78e840a4a20ec1e118f203ce54ec7
Ernani Ricoy <ernani.ricoy@engdb.com.br>
2019-08-30 14:08:44 -0300

    Angular 8 course - Section 14 (Observables)
    - Replace cross component communication using Event Emitters with
    Observables, which is the proper way. Event Emitters which use Input or
    Output decorators where not replaced, as those are used correctly.
    - Remove unuses Event Emitters.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,30 +1,31 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
 
-  private ingredients: Ingredient[] = [
-    new Ingredient('Apple', 4),
-    new Ingredient('Orange juice', 2)
-  ];
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apple', 4),
+        new Ingredient('Orange juice', 2)
+    ];
 
-  constructor() { }
+    constructor() { }
 
-  getIngredients() {
-    return this.ingredients.slice();
-  }
+    getIngredients() {
+        return this.ingredients.slice();
+    }
 
-  addIngredient(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
-  }
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
 
-  addIngredients(ingredients: Ingredient[]) {
-    this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
-  }
+    addIngredients(ingredients: Ingredient[]) {
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
 }
