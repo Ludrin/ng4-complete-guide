515eda66b08fc18d0aff2233eb6d872b5c0a48dc
Ernani Ricoy <ernani.ricoy@engdb.com.br>
2019-11-07 10:19:01 -0300

    Angular 8 course - Section 19 (Http Requests)
    - Add Data Storage service which communicates with a server in order to
    save and fetch recipes. The server is provided by Google's firebase.
    - Add functionality to the Manage buttons tab in order to call the Data
    Storage service.
    - Resolve the recipes before loading the edit or details page if there
    are no recipes in the page.
    - Remove the mock recipes created in the Recipes service.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit, } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    constructor(private dataStorageService: DataStorageService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    onSaveData() {
+        this.dataStorageService.storeRecipes();
+    }
+
+    onFetchData() {
+        this.dataStorageService.fetchRecipes().subscribe();
+    }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,28 +10,35 @@ import { Subject } from 'rxjs';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            "Pork 'n veggies",
-            'Sliced pork with vegetables',
-            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-            [
-                new Ingredient('Pork slice', 1),
-                new Ingredient('Carrot', 3)
-            ]
-        ),
-        new Recipe(
-            'Simple tagliatelle',
-            'Tagliatelle with grinded cheese',
-            'https://c.pxhere.com/photos/99/04/tagliatelle_pasta_on_a_fork_food_italian_meal_cuisine_italy_traditional-1375412.jpg!d',
-            [
-                new Ingredient('Tagliatele package', 1),
-                new Ingredient('Grinded cheese', 1)
-            ]
-        )
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         "Pork 'n veggies",
+    //         'Sliced pork with vegetables',
+    //         'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+    //         [
+    //             new Ingredient('Pork slice', 1),
+    //             new Ingredient('Carrot', 3)
+    //         ]
+    //     ),
+    //     new Recipe(
+    //         'Simple tagliatelle',
+    //         'Tagliatelle with grinded cheese',
+    //         'https://c.pxhere.com/photos/99/04/tagliatelle_pasta_on_a_fork_food_italian_meal_cuisine_italy_traditional-1375412.jpg!d',
+    //         [
+    //             new Ingredient('Tagliatele package', 1),
+    //             new Ingredient('Grinded cheese', 1)
+    //         ]
+    //     )
+    // ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private shoppingListService: ShoppingListService) { }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         return this.recipes.slice();
