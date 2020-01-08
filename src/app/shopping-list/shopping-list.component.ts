0a7a788660f7a25499c8fbe16a55af743377e980
Ernani Ricoy <ernani.ricoy@engdb.com.br>
2020-01-08 09:32:30 -0300

    Create root reducer for entire application
    - Create auth reducer which, at this point, does nothing.
    - Create reducer with shopping list and auth reducer to be accessed in
    entire application.
    - Import global reducer instead of shopping list reducer.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { Ingredient } from '../shared/ingredient.model';
-import * as fromShoppingList from './store/shopping-list.reducer';
 import * as ShoppingListActions from './store/shopping-list.actions';
+import * as fromApp from '../store/app.reducer';
 
 @Component({
     selector: 'app-shopping-list',
@@ -14,7 +14,7 @@ export class ShoppingListComponent implements OnInit {
     ingredients: Observable<{ ingredients: Ingredient[] }>;
 
     constructor(
-        private store: Store<fromShoppingList.AppState>
+        private store: Store<fromApp.AppState>
     ) { }
 
     ngOnInit() {
